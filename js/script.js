102c123bac89273155597c21e1ce3e64e41eb1a5
marianychen <myc9077@nyu.edu>
2023-11-30 14:55:12 +0800

    edits

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
-let NUM_OF_BUTTERFLY = 10;
+let NUM_OF_BUTTERFLY = 20;
 let butterflies = [];
-let r;
-let g;
-let b;
+let r, g, b;
+let rainSound;
+let flowerImg;
+let rain = [];
+let NUM_OF_RAIN = 100;
+let rainBool = false;
 let human;
+let humanImg;
+let forrestSound;
+
+function preload() {
+  rainSound = loadSound("/Users/marianchen/Desktop/snr yr fall 2023/project-b-draft/assets/lightrain.wav");
+  flowerImg = loadImage("/Users/marianchen/Desktop/snr yr fall 2023/project-b-draft/assets/doodle.png");
+  humanImg = loadImage("/Users/marianchen/Desktop/snr yr fall 2023/project-b-draft/assets/hand.png");
+  forrestSound = loadSound("/Users/marianchen/Desktop/snr yr fall 2023/project-b-draft/assets/forrestsound.wav");
+}
 
 function setup() {
-  let canvas = createCanvas(600, 600);
+  let canvas = createCanvas(1000, 600);
   canvas.parent('p5container');
-
+  
   human = new Butterfly(random(width), random(height));
-  //human.changeColor(255,0,0);
-  //human.changeSize(40);
     
   for (let i = 0; i < NUM_OF_BUTTERFLY; i++) {
     butterflies[i] = new Butterfly(random(width), random(height));
   }
 }
-
 function draw() {
-  background(50);
+  background(0, 0, 51, 90);
+  //forrestSound.loop();
+  drawStars(4);
+  
+  //moon
+  fill(246, 241, 213);
+  ellipse(width - 100, 100, 50);
+  
+   
+    drawClouds(frameCount,0);
+  
 
   // update and display
   for (let i = 0; i < butterflies.length; i++) {
@@ -38,102 +57,213 @@ function draw() {
   //human.changeSize(35);
 }
 
+function drawClouds(x, y){
+
+  fill(177, 177, 205);
+  ellipse(x,120,y + 25,30);
+  ellipse(x + 50,y + 100,40,50);
+  ellipse(x + 100,y + 110,25,30);
+  ellipse(x + 135,y + 115,25,25);
+  
+  ellipse(x + 250,y + 120,25,35);
+  ellipse(x + 300,y + 100,30,30);
+  ellipse(x + 300,y + 130,30,30);
+  ellipse(x + 345,y + 115,30,40);
+  ellipse(x + 390,y + 120,25,30);
+  
+  ellipse(x + 550,y + 120,25,35);
+  ellipse(x + 600,y + 100,30,30);
+  ellipse(x + 600,y + 130,30,30);
+  ellipse(x + 650,y + 115,30,40);
+  ellipse(x + 390,y + 120,25,30);
+  
+  
+}
+
+function drawStars(numStars) {
+  fill(255);
+  noStroke();
+  for (let i = 0; i < numStars; i++) {
+    let x = random(width);
+    let y = random(height);
+    ellipse(x, y, 2, 2);
+  }
+}
+
+function drawMoon(x, y, radius) {
+  fill(255);
+  noStroke();
+  ellipse(x, y, radius);
+}
+class Human {
+  constructor(startX, startY) {
+    this.x = startX;
+    this.y = startY;
+    this.xSpd = 0.5;
+    this.ySpd = 0.5;
+    this.dia = 25;
+  }
+
+  move() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.x += this.xSpd;
+    this.y += this.ySpd;
+  }
+
+  bounce() {
+    if (this.x > width || this.x < 0) {
+      this.xSpd = -this.xSpd;
+    }
+
+    if (this.y > height || this.y < 0) {
+      this.ySpd = -this.ySpd;
+    }
+  }
+  collisionDetect(allObjects) {
+    for (let i = 0; i < allObjects.length; i++) {
+      let p = allObjects[i];
+      let d = dist(this.x, this.y, p.x, p.y);
+      if (d < this.dia / 2 + p.dia / 2) {
+        console.log("hello");
+          allObjects.splice(i,1);
+        
+      }
+    }
+  }
+
+  display() {
+    imageMode(CENTER);
+    image(humanImg, this.x, this.y, 50, 50);
+  }
+}
 class Butterfly {
   // constructor function
   constructor(startX, startY) {
-    // r = random(255);
-    // g = random(255);
-    // b = random(255);
     this.x = startX;
     this.y = startY;
     this.xSpd = random(-3, 3);
     this.ySpd = random(-3, 3);
-    //this.hue = random(0, 377);
+    this.xAccel = random(-3, 3);
+    this.yAccel = random(-3, 3);
+    this.targetX = startX;
+    this.targetY = startY;
+    this.dia = 34;
   }
-  // methods (functions): particle's behaviors
-  move(){
-    this.x += this.xSpd;
-    this.y += this.ySpd;
-  
+
+  move() {
+    this.x += this.xSpd + this.xAccel;
+    this.y += this.ySpd + this.yAccel;
   }
-  slowDown(){
+  slowDown() {
     this.xSpd *= 0.995;
     this.ySpd *= 0.995;
   }
 
-  speedUp(){
+  speedUp() {
     this.xSpd *= 3.05;
     this.ySpd *= 3.05;
   }
-  bounce(){
-    if (this.x > width || this.x < 0){
+
+  moveTowards(targetX, targetY) {
+    this.x = targetX;
+    this.y = targetY;
+  }
+  changeColor() {
+    this.color = color(92, 0, 230);
+  }
+
+  bounce() {
+    
+    if (this.x > width || this.x < 0) {
       this.xSpd = -this.xSpd;
+      this.xAccel = -this.xAccel;
     }
 
-    if (this.y>height || this.y < 0) {
+    if (this.y > height || this.y < 0) {
       this.ySpd = -this.ySpd;
+      this.yAccel = -this.yAccel;
     }
   }
-  
-   attractTo(targetX, targetY){
-    let accelX = (targetX - this.x)* 0.1;
-    let accelY = (targetY - this.y) * 0.1;
-    this.x += accelX;
-    this.y += accelY;
-  }
-  
-  escapeFrom(other){
+
+  escapeFrom(other) {
     let d = dist(this.x, this.y, other.x, other.y);
 
-    if(d<100){
-      let accelX = (other.x - this.x)* 0.05 * -1;
-      let accelY = (other.y - this.y) * 0.05 *-1;
+    if (d < 100) {
+      let accelX = (other.x - this.x) * 0.05 * -1;
+      let accelY = (other.y - this.y) * 0.05 * -1;
       this.xSpd += accelX;
       this.ySpd += accelY;
     }
-
   }
+  
   
   display() {
     push();
-    scale(0.3);
     ellipseMode(RADIUS);
     //noStroke();
-    
+    this.drawButterfly();
+  }
+  drawButterfly() {
     //Wings
-    ellipse()
-    ellipse(this.x + 167, this.y + 180, 60, 45);
-    ellipse(this.x + 182, this.y + 240, 50, 35);
-    ellipse(this.x + 271, this.y + 185, 60, 45);
-    ellipse(this.x + 260, this.y + 245, 50, 35);
-    
+    fill(255);
+    ellipse(this.x - 17, this.y - 10, 18, 13.5);
+    ellipse(this.x - 15, this.y + 10, 15, 10.5);
+    ellipse(this.x + 17, this.y - 10, 18, 13.5);
+    ellipse(this.x + 15, this.y + 10, 15, 10.5);
+
     // Inner circles
-    //fill(mouseX, mouseY, 255);
-    ellipse(this.x + 167, this.y + 170, 25, 18);
-    ellipse(this.x + 182, this.y + 240, 20, 15);
-    ellipse(this.x + 271, this.y + 175, 25, 18);
-    ellipse(this.x + 260, this.y + 245, 20, 15);
-    
-    // body of butterfly
-    fill(0);
-   ellipse(this.x + 220, this.y + 180, 10, 10);
-    ellipse(this.x + 220, this.y + 205, 13, 20);
-    ellipse(this.x + 220, this.y + 220, 7, 50);
-  
+    fill(mouseX, mouseY, 255);
+    ellipse(this.x - 17, this.y - 10, 7.5, 5.4);
+    ellipse(this.x - 15, this.y + 10, 6, 4.5);
+    ellipse(this.x + 17, this.y - 10, 7.5, 5.4);
+    ellipse(this.x + 15, this.y + 10, 6, 4.5);
+
+    //Inner inner circles
     push();
-    // anntenae
-    angleMode(DEGREES);
-    translate(this.x + 230, this.y + 160);
-    rotate(20);
-    ellipse(0, 0, 2, 35);
-    pop();
-    push();
-    translate(this.x + 210, this.y + 160);
-    rotate(340);
-    ellipse(0, 0, 2, 35);
+    noStroke();
+    fill(this.r, this.b, this.g);
+    ellipse(this.x - 17, this.y - 10, 6.5, 2);
+    ellipse(this.x - 15, this.y + 10, 5, 1.7);
+    ellipse(this.x + 17, this.y - 10, 6.5, 2);
+    ellipse(this.x + 15, this.y + 10, 5, 1.7);
     pop();
 
-    fill(255);
+    // body of butterfly
+    fill(0);
+    ellipse(this.x, this.y + 5, 2.5, 20);
+    ellipse(this.x, this.y - 15, 5, 5);
+    ellipse(this.x, this.y + 4, 6, 12);
+
+    // anntenae
+
+    push();
+    strokeWeight(2);
+    stroke(0);
+    line(this.x, this.y - 20, this.x - 10, this.y - 40);
+    line(this.x, this.y - 20, this.x + 10, this.y - 40);
+    pop();
+  }
+}
+
+class Rain {
+  constructor(startX, startY) {
+    this.x = startX;
+    this.y = startY;
+    this.size = random(5, 10);
+    this.spd = random(10, 20);
+  }
+  update() {
+    this.y += this.spd;
+    if (this.y > height) {
+      this.y = 0;
+      this.x = random(width);
+    }
+  }
+  display() {
+    push();
+    fill(146, 186, 210);
+    ellipse(this.x, this.y, 2, 5);
     pop();
   }
 }
